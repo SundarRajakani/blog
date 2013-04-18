b28ad70a82c37df63692ac6e7e66739f922ef834
duncky <d@duncky.com>
2013-04-18 21:59:00 +0100

    changed api to index

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
 $(function () {
+
+  console.log('test');
+
+
+
   $.ajax({
-      url: 'http://api.dunckr.com/get',
-      dataType: 'jsonp',
-      success: function (data) {
-        console.log(data);
-      }
-    });
+   url: 'http://api.dunckr.com/get',
+   dataType: 'jsonp',
+   type: 'POST',
+   data: '',
+   contentType: "application/json; charset=utf-8",
+   // success: function(data) {
+   //  console.log('here');
+   // },
+
+   complete: function(xhr,stat) {
+    console.log(stat);
+   }
+
+  });
+
+
 });
