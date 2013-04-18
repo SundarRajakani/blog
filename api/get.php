1270d751bf04142b5bbea67cdf6edc694436255f
duncky <d@duncky.com>
2013-04-18 20:28:47 +0100

    rearranging api

---
+++ api/get.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once('../config/config.php');
+
+try {
+	$conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+
+	$sth = $conn->query('select * from posts');
+
+	$sth->setFetchMode(PDO::FETCH_ASSOC);
+
+	$out = array();
+
+	while ($row = $sth->fetch() ) {
+		$out[] = $row;
+	}
+
+	return json_encode($out);
+
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
+
+
+?>
