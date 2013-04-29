4a748c5582e8c9deb0ac6e11721010d32cf76a6d
duncky <d@duncky.com>
2013-04-29 23:53:26 +0100

    forgot to build

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -91,49 +91,9 @@ window.require.register("application", function(exports, require, module) {
         var Blog     = require('/models/blog'),
             BlogView = require('/views/blogview');
 
-        var content = [
-         {
-          id:     '1',
-          title:    'first post',
-          description:'this is my first post',
-          thumb:    '../assets/images/glyphicons-halflings-white.png',
-          content:  '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p>',
-          type:   [
-                  'general',
-                  'programming'
-                ],
-          date:   '1st of Jan'
-         },
-         {
-          id:     '2',
-          title:    'second post',
-          description:'post number #2',
-          thumb:    '../assets/images/glyphicons-halflings-white.png',
-          content:  '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p>',
-          type:   [
-                  'javascript',
-                  'programming'
-                ],
-          date:   '1st of Feb'
-         },
-         {
-          id:     '3',
-          title:    'third post',
-          description:'posting again!!!',
-          thumb:    '../assets/images/glyphicons-halflings-white.png',
-          content:  '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ut, animi, distinctio aperiam sed molestias culpa officiis molestiae reiciendis ipsa harum laudantium expedita voluptatem saepe minus nisi dolor. Assumenda, officiis!</p>',
-          type:   [
-                  'general',
-                  'news'
-                ],
-          date:   '1st of March'
-         }
-
-        ];
-
         var headerView = new HeaderView();
 
-        var blog = new Blog(content);
+        var blog = new Blog();
         var blogView = new BlogView({ collection: blog });
 
         var footerView = new FooterView();
@@ -176,7 +136,9 @@ window.require.register("models/blog", function(exports, require, module) {
 
   module.exports = Backbone.Collection.extend({
 
-    model: Post
+    model: Post,
+
+    url: '/api/get'
 
   });
   
@@ -238,6 +200,7 @@ window.require.register("views/blogview", function(exports, require, module) {
     template: Template,
 
     initialize: function() {
+      this.collection.fetch();
       this.render();
     },
 
