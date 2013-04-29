c1dc2aeb9eec6284d3185b5a0c40287717b26f74
duncky <d@duncky.com>
2013-04-29 22:36:25 +0100

    Fixing API

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = '184.172.153.64';
+$database = 'duncky_dunckr';
+$user = 'duncky_pub';
+$pass = '{u:X/k.M#]P<bY693Q7JF8Dy(^2L';
+
+// $user = 'duncky';
+// $pass = 'XsKVb5MnOWCh';
+
+
+?>
