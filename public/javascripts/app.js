7d97fe63207295ddf1387e0210f578b634569f06
duncky <d@duncky.com>
2013-05-04 10:57:58 +0100

    brunch b

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,317 @@
-(function(){"use strict";var t="undefined"!=typeof window?window:global;if("function"!=typeof t.require){var e={},n={},i=function(t,e){return{}.hasOwnProperty.call(t,e)},r=function(t,e){var n,i,r=[];n=/^\.\.?(\/|$)/.test(e)?[t,e].join("/").split("/"):e.split("/");for(var o=0,s=n.length;s>o;o++)i=n[o],".."===i?r.pop():"."!==i&&""!==i&&r.push(i);return r.join("/")},o=function(t){return t.split("/").slice(0,-1).join("/")},s=function(e){return function(n){var i=o(e),s=r(i,n);return t.require(s)}},a=function(t,e){var i={id:t,exports:{}};e(i.exports,s(t),i);var r=n[t]=i.exports;return r},u=function(t){var o=r(t,".");if(i(n,o))return n[o];if(i(e,o))return a(o,e[o]);var s=r(o,"./index");if(i(n,s))return n[s];if(i(e,s))return a(s,e[s]);throw Error('Cannot find module "'+t+'"')},l=function(t,n){if("object"==typeof t)for(var r in t)i(t,r)&&(e[r]=t[r]);else e[t]=n};t.require=u,t.require.define=l,t.require.register=l,t.require.brunch=!0}})(),window.require.register("application",function(t,e,n){Application={initialize:function(){var t=e("/views/headerview"),n=e("/views/footerview"),i=e("/models/blog"),r=e("/views/blogview"),o=new i,s=o.fetch();s.done(function(){console.log(o),new r({collection:o})}),new t,new n}},n.exports=Application}),window.require.register("initialize",function(t,e){var n=e("application");$(function(){n.initialize(),Backbone.history.start()})}),window.require.register("lib/router",function(t,e,n){e("application"),n.exports=Backbone.Router.extend({routes:{"":"home"},home:function(){}})}),window.require.register("lib/view_helper",function(){}),window.require.register("models/blog",function(t,e,n){var i=e("./post");n.exports=Backbone.Collection.extend({model:i,url:"/api/get"})}),window.require.register("models/post",function(t,e,n){n.exports=Backbone.Model.extend({defaults:{}})}),window.require.register("views/blogview",function(t,e,n){var i=e("./templates/blog"),r=e("/views/postview");n.exports=Backbone.View.extend({el:$("#blog"),template:i,initialize:function(){this.render()},render:function(){return this.$el.html(this.template),_.each(this.collection.models,function(t){console.log(t),this.renderEach(t)},this),this},renderEach:function(t){var e=new r({model:t});$(this.el).append(e.render().el)}})}),window.require.register("views/footerview",function(t,e,n){var i=e("./templates/footer");n.exports=Backbone.View.extend({el:$("footer"),template:i,initialize:function(){this.render()},render:function(){return this.$el.html(this.template),this}})}),window.require.register("views/headerview",function(t,e,n){var i=e("./templates/header");n.exports=Backbone.View.extend({el:$("header"),template:i,initialize:function(){this.render()},render:function(){return this.$el.html(this.template),this}})}),window.require.register("views/postview",function(t,e,n){var i=e("./templates/post");n.exports=Backbone.View.extend({className:"post",template:i,render:function(){return this.$el.html(this.template(this.model.toJSON())),this}})}),window.require.register("views/templates/blog",function(t,e,n){n.exports=Handlebars.template(function(t,e,n,i,r){return this.compilerInfo=[2,">= 1.0.0-rc.3"],n=n||t.helpers,r=r||{},"<!--  -->"})}),window.require.register("views/templates/footer",function(t,e,n){n.exports=Handlebars.template(function(t,e,n,i,r){return this.compilerInfo=[2,">= 1.0.0-rc.3"],n=n||t.helpers,r=r||{},' <div class="footbar navbar navbar-fixed-bottom">\n 	<p class="nav dis">The opinions stated here are my own and not those of my employers.</p>\n    <p class="nav pull-right copy">©duncanbeaton.com</p>\n</div>'})}),window.require.register("views/templates/header",function(t,e,n){n.exports=Handlebars.template(function(t,e,n,i,r){return this.compilerInfo=[2,">= 1.0.0-rc.3"],n=n||t.helpers,r=r||{},'<row>\n  <div id="logo">dunckr*</div>\n</row>\n<row>\n  <div class="menu-bar">\n\n    <a href="https://twitter.com/dunckr" alt="@dunckr"><i class="icon-twitter icon-4x"></i>\n</a>\n    <a href="http://github.com/dunckr" alt="github/dunckr"><i class="icon-github icon-4x"></i></a>\n    <a href="mailto:d" alt="Message"><i class="icon-envelope-alt icon-4x"></i></a>  \n  </div>\n</row>\n'})}),window.require.register("views/templates/post",function(t,e,n){n.exports=Handlebars.template(function(t,e,n,i,r){function o(t){var e="";return e+="\n		<li>"+l(typeof t===u?t.apply(t):t)+"</li>\n		"}this.compilerInfo=[2,">= 1.0.0-rc.3"],n=n||t.helpers,r=r||{};var s,a="",u="function",l=this.escapeExpression,c=this;return a+='<div class="content span8">\n	<h1>',(s=n.title)?s=s.call(e,{hash:{},data:r}):(s=e.title,s=typeof s===u?s.apply(e):s),a+=l(s)+"</h1>\n	<p>",(s=n.date)?s=s.call(e,{hash:{},data:r}):(s=e.date,s=typeof s===u?s.apply(e):s),a+=l(s)+"</p>\n	<p>",(s=n.content)?s=s.call(e,{hash:{},data:r}):(s=e.content,s=typeof s===u?s.apply(e):s),(s||0===s)&&(a+=s),a+="</p>\n<!--  	<ul> ",s=n.each.call(e,e.type,{hash:{},inverse:c.noop,fn:c.program(1,o,r),data:r}),(s||0===s)&&(a+=s),a+="\n	</ul>\n -->\n</div>"})});
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    definition(module.exports, localRequire(name), module);
+    var exports = cache[name] = module.exports;
+    return exports;
+  };
+
+  var require = function(name) {
+    var path = expand(name, '.');
+
+    if (has(cache, path)) return cache[path];
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex];
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '"');
+  };
+
+  var define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.register = define;
+  globals.require.brunch = true;
+})();
+
+window.require.register("application", function(exports, require, module) {
+  // Application bootstrapper.
+  Application = {
+      initialize: function() {
+
+        var HeaderView = require('/views/headerview'),
+            FooterView = require('/views/footerview');
+
+
+        var Blog     = require('/models/blog'),
+            BlogView = require('/views/blogview');
+
+        var blog = new Blog();
+        var p = blog.fetch();
+        p.done(function() {
+          console.log(blog);
+          var blogView = new BlogView({ collection: blog});
+        });
+
+
+        // var json = {"id":"1","title":"Hello World","description":"<b>Duis aute<\/b> irure dolor in reprehenderit in voluptate velit esse cillum Dolore eu fugiat nulla pariatur.","content":"<h1>Lorem ipsum dolor sit amet</h1> <p>consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <span>commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Exc</span>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea epteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n<p>Nostrud exercitation ullamco laboris nisi ut aliquip ex ea epteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>","thumb":"blank","date":"2013-04-03 14:30:31","category":"test"};
+        // var blog = new Blog(json);
+        // var blogView = new BlogView({ collection: blog});
+
+
+        var headerView = new HeaderView();
+        var footerView = new FooterView();
+      }
+  };
+
+  module.exports = Application;
+  
+});
+window.require.register("initialize", function(exports, require, module) {
+  var application = require('application')
+
+  $(function() {
+      application.initialize()
+      Backbone.history.start()
+  })
+  
+});
+window.require.register("lib/router", function(exports, require, module) {
+  var application = require('application');
+
+  module.exports = Backbone.Router.extend({
+      routes: {
+          '': 'home'
+          // get particuarl post id
+      },
+
+      home: function() {
+          // $('body').html(application.homeView.render().el)
+      }
+  });
+  
+});
+window.require.register("lib/view_helper", function(exports, require, module) {
+  
+});
+window.require.register("models/blog", function(exports, require, module) {
+  var Post = require('./post');
+
+  module.exports = Backbone.Collection.extend({
+
+    model: Post,
+
+    url: '/api/get'
+
+  });
+  
+});
+window.require.register("models/post", function(exports, require, module) {
+  
+  module.exports = Backbone.Model.extend({
+
+    defaults: {
+      
+    }
+
+  });
+});
+window.require.register("views/blogview", function(exports, require, module) {
+  var Template = require('./templates/blog'),
+      PostView = require('/views/postview');
+
+  module.exports = Backbone.View.extend({
+
+    el: $('#blog'),
+
+    template: Template,
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template);
+      _.each(this.collection.models, function(item) {
+        console.log(item);
+        this.renderEach(item);
+      }, this);
+      return this;
+    },
+
+    renderEach: function(item) {
+      var postView = new PostView({ model: item });
+      $(this.el).append(postView.render().el);
+    }
+
+  });
+  
+});
+window.require.register("views/footerview", function(exports, require, module) {
+  var Template = require('./templates/footer');
+
+  module.exports = Backbone.View.extend({
+
+    el: $('footer'),
+
+    template: Template,
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template);
+      return this;
+    }
+
+  });
+  
+});
+window.require.register("views/headerview", function(exports, require, module) {
+  var Template = require('./templates/header');
+
+  module.exports = Backbone.View.extend({
+
+    el: $('header'),
+
+    template: Template,
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template);
+      return this;
+    }
+
+  });
+  
+});
+window.require.register("views/postview", function(exports, require, module) {
+  var Template = require('./templates/post');
+
+  module.exports = Backbone.View.extend({
+
+    className: 'post',
+
+    template: Template,
+
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+
+  });
+  
+});
+window.require.register("views/templates/blog", function(exports, require, module) {
+  module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+    this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+  helpers = helpers || Handlebars.helpers; data = data || {};
+    
+
+
+    return "<!--  -->";
+    });
+});
+window.require.register("views/templates/footer", function(exports, require, module) {
+  module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+    this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+  helpers = helpers || Handlebars.helpers; data = data || {};
+    
+
+
+    return " <div class=\"footbar navbar navbar-fixed-bottom\">\n 	<p class=\"nav dis\">The opinions stated here are my own and not those of my employers.</p>\n    <p class=\"nav pull-right copy\">©duncanbeaton.com</p>\n</div>";
+    });
+});
+window.require.register("views/templates/header", function(exports, require, module) {
+  module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+    this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+  helpers = helpers || Handlebars.helpers; data = data || {};
+    
+
+
+    return "<row>\n  <div id=\"logo\">dunckr*</div>\n</row>\n<row>\n  <div class=\"menu-bar\">\n\n    <a href=\"https://twitter.com/dunckr\" alt=\"@dunckr\"><i class=\"icon-twitter icon-4x\"></i>\n</a>\n    <a href=\"http://github.com/dunckr\" alt=\"github/dunckr\"><i class=\"icon-github icon-4x\"></i></a>\n    <a href=\"mailto:d\" alt=\"Message\"><i class=\"icon-envelope-alt icon-4x\"></i></a>  \n  </div>\n</row>\n";
+    });
+});
+window.require.register("views/templates/post", function(exports, require, module) {
+  module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+    this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+  helpers = helpers || Handlebars.helpers; data = data || {};
+    var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+  function program1(depth0,data) {
+    
+    var buffer = "";
+    buffer += "\n		<li>"
+      + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
+      + "</li>\n		";
+    return buffer;
+    }
+
+    buffer += "<div class=\"content span8\">\n	<h1>";
+    if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+    else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+    buffer += escapeExpression(stack1)
+      + "</h1>\n	<p>";
+    if (stack1 = helpers.date) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+    else { stack1 = depth0.date; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+    buffer += escapeExpression(stack1)
+      + "</p>\n	<p>";
+    if (stack1 = helpers.content) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+    else { stack1 = depth0.content; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+    if(stack1 || stack1 === 0) { buffer += stack1; }
+    buffer += "</p>\n<!--  	<ul> ";
+    stack1 = helpers.each.call(depth0, depth0.type, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+    if(stack1 || stack1 === 0) { buffer += stack1; }
+    buffer += "\n	</ul>\n -->\n</div>";
+    return buffer;
+    });
+});
