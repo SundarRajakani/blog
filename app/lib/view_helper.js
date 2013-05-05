b384dfd5464bbc09be3e5f5bb1ca9b5497235fc4
duncky <d@duncky.com>
2013-05-05 14:17:33 +0100

    debugging date

--- app/lib/view_helper.js
+++ app/lib/view_helper.js
@@ -1,6 +1,9 @@
 Handlebars.registerHelper('dateFormat', function(timestamp) {
   var date = new Date(timestamp);
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'];
+  console.log(timestamp);
+  console.log(months);
+  console.log(date);
   return date.getDay() + ' ' +
          months[date.getMonth()] + ' ' +
          date.getFullYear();
