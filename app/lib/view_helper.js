d4c5fc6bd450f2707b0c2a7039079bdc946c22ab
duncky <d@duncky.com>
2013-05-05 14:25:51 +0100

    var name err

--- app/lib/view_helper.js
+++ app/lib/view_helper.js
@@ -1,6 +1,6 @@
 Handlebars.registerHelper('dateFormat', function(timestamp) {
   var a = timestamp.split(/[^0-9]/);
-  var d=new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5] );
+  var date = new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5] );
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'];
   console.log(timestamp);
   console.log(months);
